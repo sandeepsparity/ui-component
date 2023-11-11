6947d5299b199dd1da82380ff685b7c842c70762
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 13:53:14 -0600

    Create StepperVertical.d.ts

---
+++ uds/molecule/Stepper/StepperVertical/StepperVertical.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { StepperVerticalProps } from './StepperVertical.types';
+import '../../../tailwind.css';
+declare const StepperVertical: (props: StepperVerticalProps) => React.JSX.Element;
+export default StepperVertical;
