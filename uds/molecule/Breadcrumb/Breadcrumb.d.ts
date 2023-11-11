c128b15c8f747b2fa076c7cc670421d5add8792e
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 02:10:40 -0600

    Create Breadcrumb.d.ts

---
+++ uds/molecule/Breadcrumb/Breadcrumb.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { BreadcrumbProps } from './Breadcrumb.types';
+import '../../tailwind.css';
+declare const Breadcrumb: (props: BreadcrumbProps) => React.JSX.Element;
+export default Breadcrumb;
