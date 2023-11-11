8926e09278658c00cbe6e0ba5f058df742cf7479
Sandeep Reddy <sandeep.sparity@gmail.com>
2023-11-10 23:40:15 -0600

    Create Slider.d.ts

---
+++ uds/atom/component/Slider/Slider.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { SliderProps } from './Slider.types';
+import '../../../tailwind.css';
+declare const Slider: (props: SliderProps) => React.JSX.Element;
+export default Slider;
