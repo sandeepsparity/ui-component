8a84c71f18207db1f447cede60789cdfb50b5b8d
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 03:03:05 -0600

    Create Pagination.d.ts

---
+++ uds/molecule/Pagination/Pagination.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { PaginationProps } from './Pagination.types';
+import '../../tailwind.css';
+declare const Pagination: (props: PaginationProps) => React.JSX.Element;
+export default Pagination;
