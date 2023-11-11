26776606a26b8e406a37922df39f3ceeae8992e2
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 02:53:37 -0600

    Create Modal.d.ts

---
+++ uds/molecule/Modal/Modal.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { ModalProps } from './Modal.types';
+import '../../tailwind.css';
+declare const Modal: (props: ModalProps) => React.JSX.Element;
+export default Modal;
