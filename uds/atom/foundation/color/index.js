7e0d53f9c4715dfd469ca0ed9efec69234560ce1
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 01:37:30 -0600

    Create index.js

---
+++ uds/atom/foundation/color/index.js
@@ -0,0 +1,7 @@
+import { __assign } from 'tslib';
+import deprecated from './colors_deprecated.json.js';
+import colors$1 from './colors.json.js';
+
+var colors = __assign(__assign({}, deprecated), colors$1);
+
+export { colors as default };
