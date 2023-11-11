e7f26328899e587152558a4327a55b2dec23e4fa
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 12:52:21 -0600

    Create RadioGroup.d.ts

---
+++ uds/molecule/Radio/RadioGroup/RadioGroup.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { RadioGroupProps } from './RadioGroup.types';
+import '../../../tailwind.css';
+declare const RadioGroup: <T>(props: RadioGroupProps<T>) => React.JSX.Element;
+export default RadioGroup;
