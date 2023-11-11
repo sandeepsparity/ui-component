d57d2bae9018b1b379df47618053c1527cea511e
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 13:59:39 -0600

    Create TabBar.d.ts

---
+++ uds/molecule/TabBar/TabBar.d.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+import { TabBarProps } from './TabBar.types';
+import Tab from './Tab';
+import '../../tailwind.css';
+declare const TabBar: (props: TabBarProps) => React.JSX.Element;
+export default TabBar;
+export { Tab };
