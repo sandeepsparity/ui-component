212a648d85805248be04a71cad4a5dc9f2680157
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 13:48:34 -0600

    Create Slug.types.d.ts

---
+++ uds/molecule/Slug/Slug.types.d.ts
@@ -0,0 +1,11 @@
+import { ReactElement } from 'react';
+export interface SlugProps {
+    icon?: ReactElement;
+    title?: string;
+    label?: string;
+    backgroundColor?: string;
+    borderColor?: string;
+    titleColor?: string;
+    labelColor?: string;
+    className?: string;
+}
