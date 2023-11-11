9548b239f0f6fe8150452cb5273ccbdac64ae27d
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 02:25:52 -0600

    Create Divider.types.d.ts

---
+++ uds/molecule/Divider/Divider.types.d.ts
@@ -0,0 +1,7 @@
+import { BaseProps } from '../../atom/interface/BaseProps.types';
+export interface DividerProps extends BaseProps {
+    color?: string;
+    width?: number;
+    height?: number;
+    className?: string;
+}
