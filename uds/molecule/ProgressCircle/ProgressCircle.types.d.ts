2dd8f38c768c8bfb1caaea5e99dd32959ca0ddda
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 03:11:11 -0600

    Create ProgressCircle.types.d.ts

---
+++ uds/molecule/ProgressCircle/ProgressCircle.types.d.ts
@@ -0,0 +1,11 @@
+export interface ProgressCircleProps {
+    className?: string;
+    circleWidth?: number;
+    progressPercentage?: number;
+    strokeWidth?: number;
+    primaryColor?: string;
+    secondaryColor?: string;
+    textSize?: number;
+    hidePercentage?: boolean;
+    textColor?: string;
+}
