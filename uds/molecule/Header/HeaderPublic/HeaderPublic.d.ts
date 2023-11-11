c4f6aa4e7e816ecdc373d9b2c43472a8bc6091f4
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 02:41:52 -0600

    Create HeaderPublic.d.ts

---
+++ uds/molecule/Header/HeaderPublic/HeaderPublic.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { HeaderPublicProps } from './HeaderPublic.types';
+import '../../../tailwind.css';
+declare const HeaderPublic: (props: HeaderPublicProps) => React.JSX.Element;
+export default HeaderPublic;
