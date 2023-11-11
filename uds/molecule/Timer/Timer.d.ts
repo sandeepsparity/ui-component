ad6be80a0c79d8811933202176ceb9390089ae59
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 14:35:03 -0600

    Create Timer.d.ts

---
+++ uds/molecule/Timer/Timer.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { TimerProps } from './Timer.types';
+import '../../tailwind.css';
+declare const Timer: (props: TimerProps) => React.JSX.Element | null;
+export default Timer;
