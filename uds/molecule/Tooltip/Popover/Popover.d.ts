5378a204967eb7e223bcab0dffd61b551e645b2d
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 14:39:00 -0600

    Create Popover.d.ts

---
+++ uds/molecule/Tooltip/Popover/Popover.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { PopoverProps } from './Popover.types';
+import '../../../tailwind.css';
+declare const Popover: (props: PopoverProps) => React.JSX.Element;
+export default Popover;
