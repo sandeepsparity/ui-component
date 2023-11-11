6a5fd1c861d2f68774427b146c198a98d26aea61
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 01:29:42 -0600

    Create AlbertsonsFullLogo.d.ts

---
+++ uds/atom/logo/AlbertsonsFullLogo/AlbertsonsFullLogo.d.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+import { LogoProps } from '../Logo.types';
+declare const AlbertsonsFullLogo: (props: LogoProps) => React.JSX.Element;
+export default AlbertsonsFullLogo;
