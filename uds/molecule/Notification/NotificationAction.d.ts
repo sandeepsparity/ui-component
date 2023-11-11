49ec712f7aedb64691420f5b35aa2536d2d3834c
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 02:56:46 -0600

    Create NotificationAction.d.ts

---
+++ uds/molecule/Notification/NotificationAction.d.ts
@@ -0,0 +1,3 @@
+import React from 'react';
+import { NotificationActionProps } from './Notification.types';
+export declare const NotificationAction: ({ children, actionText, onActionClick, ...notification }: NotificationActionProps) => React.JSX.Element;
