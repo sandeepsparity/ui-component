263f2621468225d0dce70de4bf7e7beee35c1d4b
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 14:28:41 -0600

    Create TextArea.d.ts

---
+++ uds/molecule/TextArea/TextArea.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { TextAreaProps } from './TextArea.types';
+import '../../tailwind.css';
+declare const TextArea: (props: TextAreaProps) => React.JSX.Element;
+export default TextArea;
