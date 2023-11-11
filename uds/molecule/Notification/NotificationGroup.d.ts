801b054df0e89b6fd438c454cdc8b275c97832c3
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 02:58:47 -0600

    Create NotificationGroup.d.ts

---
+++ uds/molecule/Notification/NotificationGroup.d.ts
@@ -0,0 +1,3 @@
+import React from 'react';
+import { NotificationGroupProps } from './Notification.types';
+export declare const NotificationGroup: ({ children, subNotifications }: NotificationGroupProps) => React.JSX.Element;
