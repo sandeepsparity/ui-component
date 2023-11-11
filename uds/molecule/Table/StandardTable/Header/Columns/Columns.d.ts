758b22a6df0b9945337025386e46f4c333900097
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 14:13:28 -0600

    Create Columns.d.ts

---
+++ uds/molecule/Table/StandardTable/Header/Columns/Columns.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { ColumnsProps } from './Columns.types';
+import '../../../../../tailwind.css';
+declare const Columns: <T>(props: ColumnsProps<T>) => React.JSX.Element;
+export default Columns;
