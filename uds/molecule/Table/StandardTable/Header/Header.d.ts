95a86f0ce3fe0d2bd94c54ee8ed95401ea9508a0
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 14:15:10 -0600

    Create Header.d.ts

---
+++ uds/molecule/Table/StandardTable/Header/Header.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { HeaderProps } from './Header.types';
+import '../../../../tailwind.css';
+declare const Header: <T>(props: HeaderProps<T>) => React.JSX.Element;
+export default Header;
