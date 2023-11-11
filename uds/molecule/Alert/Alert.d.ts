31f86a73fbd2dc6b30c07f92397aa9f0d67c6bed
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 01:49:56 -0600

    Create Alert.d.ts

---
+++ uds/molecule/Alert/Alert.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { AlertProps } from './Alert.types';
+import '../../tailwind.css';
+declare const Alert: (props: AlertProps) => React.JSX.Element | null;
+export default Alert;
