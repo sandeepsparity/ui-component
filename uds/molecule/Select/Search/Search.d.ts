b2784cee364a1322741df87446a537167dd849ab
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 13:18:17 -0600

    Create Search.d.ts

---
+++ uds/molecule/Select/Search/Search.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { SearchProps } from './Search.types';
+import '../../../tailwind.css';
+declare const Search: React.ForwardRefExoticComponent<SearchProps & React.RefAttributes<HTMLDivElement>>;
+export default Search;
