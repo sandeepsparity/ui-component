cc289f0a82ccd77e4d3a1b39ff7708709e5703b5
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 14:36:43 -0600

    Create Toggle.d.ts

---
+++ uds/molecule/Toggle/Toggle.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { ToggleProps } from './Toggle.types';
+import '../../tailwind.css';
+declare const Toggle: (props: ToggleProps) => React.JSX.Element;
+export default Toggle;
