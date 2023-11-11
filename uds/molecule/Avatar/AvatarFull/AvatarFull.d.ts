e19406f1c6538e90929be91634f34fba943b28bb
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 02:01:06 -0600

    Create AvatarFull.d.ts

---
+++ uds/molecule/Avatar/AvatarFull/AvatarFull.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { AvatarFullProps } from './AvatarFull.types';
+import '../../../tailwind.css';
+declare const AvatarFull: (props: AvatarFullProps) => React.JSX.Element;
+export default AvatarFull;
