4a740a7f9a257a094fa300ac1c9c876440e67505
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 13:47:57 -0600

    Create Slug.d.ts

---
+++ uds/molecule/Slug/Slug.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { SlugProps } from './Slug.types';
+import '../../tailwind.css';
+declare const Slug: (props: SlugProps) => React.JSX.Element;
+export default Slug;
