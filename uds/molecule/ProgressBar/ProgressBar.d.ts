9d5334efb806f1738ced4432ef680c8ed6dc5d42
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 03:08:52 -0600

    Create ProgressBar.d.ts

---
+++ uds/molecule/ProgressBar/ProgressBar.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { ProgressBarProps } from './ProgressBar.types';
+import '../../tailwind.css';
+declare const ProgressBar: (props: ProgressBarProps) => React.JSX.Element;
+export default ProgressBar;
