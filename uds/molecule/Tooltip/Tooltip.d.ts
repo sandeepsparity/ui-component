22bf22710b024a3a82cfb1bbaa9f463ed0634371
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 14:40:40 -0600

    Create Tooltip.d.ts

---
+++ uds/molecule/Tooltip/Tooltip.d.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+import { PopoverProps, TooltipProps } from './Tooltip.types';
+import '../../tailwind.css';
+declare const _default: (({ children, zIndex, ...props }: TooltipProps) => React.JSX.Element) & {
+    Popover: (props: PopoverProps) => React.ReactElement<any, string | React.JSXElementConstructor<any>>;
+};
+export default _default;
