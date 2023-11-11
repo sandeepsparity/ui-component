308a59de086e10d7675295b602077a856f8a069a
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 14:16:46 -0600

    Create StandardTable.d.ts

---
+++ uds/molecule/Table/StandardTable/StandardTable.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { StandardTableProps } from './StandardTable.types';
+import '../../../tailwind.css';
+declare const StandardTable: <T>(props: StandardTableProps<T>) => React.JSX.Element;
+export default StandardTable;
