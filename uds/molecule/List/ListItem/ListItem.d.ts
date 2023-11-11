12bf952170e66d7d1be76e97e3f43bb774accb84
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 02:49:42 -0600

    Create ListItem.d.ts

---
+++ uds/molecule/List/ListItem/ListItem.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { ListItemProps } from './ListItem.types';
+import '../../../tailwind.css';
+declare const ListItem: (props: ListItemProps) => React.JSX.Element;
+export default ListItem;
