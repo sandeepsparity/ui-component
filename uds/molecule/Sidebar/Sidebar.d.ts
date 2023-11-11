27089bec4bfa9764bfcebacf484cdd6e3d4219f8
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 13:26:47 -0600

    Create Sidebar.d.ts

---
+++ uds/molecule/Sidebar/Sidebar.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { SidebarProps } from './Sidebar.types';
+import '../../tailwind.css';
+declare const Sidebar: (props: SidebarProps) => React.JSX.Element;
+export default Sidebar;
