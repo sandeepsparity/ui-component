994edc78fa28e1ae9c45d7d13c085f85643eebce
Sandeep Reddy <sandeep.sparity@gmail.com>
2023-11-10 23:34:44 -0600

    Create Label.d.ts

---
+++ uds/atom/component/Label/Label.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { LabelProps } from './Label.types';
+import '../../../tailwind.css';
+declare const Label: (props: LabelProps) => React.JSX.Element;
+export default Label;
