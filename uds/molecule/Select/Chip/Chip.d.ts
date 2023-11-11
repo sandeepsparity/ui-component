df252cbde7644e6fc5b01d04b20846b44bebf186
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 12:59:54 -0600

    Update Chip.d.ts

--- uds/molecule/Select/Chip/Chip.d.ts
+++ uds/molecule/Select/Chip/Chip.d.ts
@@ -1,1 +1,5 @@
-
+import React from 'react';
+import { ChipProps } from './Chip.types';
+import '../../../tailwind.css';
+declare const Chip: (props: ChipProps) => React.JSX.Element;
+export default Chip;
