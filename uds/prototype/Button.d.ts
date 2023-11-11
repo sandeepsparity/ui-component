3e0f3de7cb12aacb2f1ee8199488073e7d491a68
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 01:42:30 -0600

    Create Button.d.ts

---
+++ uds/prototype/Button.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { ButtonProps } from './Button.types';
+import '../../tailwind.css';
+declare const Button: (props: ButtonProps) => React.JSX.Element;
+export default Button;
