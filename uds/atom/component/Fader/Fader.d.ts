9797ae6d3c0b81086cc9c7addd9ad5377b02e737
Sandeep Reddy <sandeep.sparity@gmail.com>
2023-11-10 23:28:43 -0600

    Create Fader.d.ts

---
+++ uds/atom/component/Fader/Fader.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { FaderProps } from './Fader.types';
+import '../../../tailwind.css';
+declare const Fader: (props: FaderProps) => React.JSX.Element;
+export default Fader;
