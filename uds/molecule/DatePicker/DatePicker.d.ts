e2e032c5e2bd5a801aa09b8bb959435a8f7dcf76
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 02:23:01 -0600

    Create DatePicker.d.ts

---
+++ uds/molecule/DatePicker/DatePicker.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { DatePickerProps } from './DatePicker.types';
+import '../../tailwind.css';
+declare const DatePicker: (props: DatePickerProps) => React.JSX.Element;
+export default DatePicker;
