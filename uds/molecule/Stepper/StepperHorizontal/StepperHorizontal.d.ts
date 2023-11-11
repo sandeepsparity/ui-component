9c082bc00b564f1046b3103ea7acc838f67bc745
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 13:51:48 -0600

    Create StepperHorizontal.d.ts

---
+++ uds/molecule/Stepper/StepperHorizontal/StepperHorizontal.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { StepperHorizontalProps } from './StepperHorizontal.types';
+import '../../../tailwind.css';
+declare const StepperHorizontal: (props: StepperHorizontalProps) => React.JSX.Element;
+export default StepperHorizontal;
