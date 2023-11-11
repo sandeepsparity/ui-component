aa42b54990b64ace741627676cd139e05d383096
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 02:25:15 -0600

    Create Divider.d.ts

---
+++ uds/molecule/Divider/Divider.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { DividerProps } from './Divider.types';
+import '../../tailwind.css';
+declare const Divider: (props: DividerProps) => React.JSX.Element;
+export default Divider;
