7903bf3050bbe342609b309e531a5696a7c7d4d5
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 13:41:02 -0600

    Create InProgress.d.ts

---
+++ uds/molecule/Slug/InProgress/InProgress.d.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+import { InProgressProps } from './InProgress.types';
+declare const InProgress: (props: InProgressProps) => React.JSX.Element;
+export default InProgress;
