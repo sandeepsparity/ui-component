267af8744f159230822a9de0d8a6119dee0ee59e
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 13:29:18 -0600

    Create SkeletonLoader.d.ts

---
+++ uds/molecule/SkeletonLoader/SkeletonLoader.d.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+import { SkeletonLoaderProps } from './SkeletonLoader.types';
+import '../../tailwind.css';
+import 'react-loading-skeleton/dist/skeleton.css';
+declare const SkeletonLoader: (props: SkeletonLoaderProps) => React.JSX.Element;
+export default SkeletonLoader;
