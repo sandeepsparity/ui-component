9fbcc7f035d9287b4d5ce1c575424324ff096f1b
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 02:36:39 -0600

    Create Footer.d.ts

---
+++ uds/molecule/Footer/Footer.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { FooterProps } from './Footer.types';
+import '../../tailwind.css';
+declare const Footer: (props: FooterProps) => React.JSX.Element;
+export default Footer;
