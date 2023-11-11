d030ecf4daaa1de094aaf9abbf2e1a47263958eb
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 13:56:09 -0600

    Create Stepper.d.ts

---
+++ uds/molecule/Stepper/Stepper.d.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+import { StepperProps, StepProps } from './Stepper.types';
+import '../../tailwind.css';
+declare const _default: ((props: StepperProps) => React.JSX.Element) & {
+    Step: (props: StepProps) => React.ReactElement<any, string | React.JSXElementConstructor<any>>;
+};
+export default _default;
