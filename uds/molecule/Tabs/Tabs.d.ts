3e6671acdfb10298dce985686caf7837cf1fe854
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 14:23:36 -0600

    Create Tabs.d.ts

---
+++ uds/molecule/Tabs/Tabs.d.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+import { TabsProps } from './Tabs.types';
+import Tab from './Tab';
+import '../../tailwind.css';
+declare const Tabs: (props: TabsProps) => React.JSX.Element;
+export default Tabs;
+export { Tab };
