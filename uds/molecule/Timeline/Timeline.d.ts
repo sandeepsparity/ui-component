30610c9f074ce5fdf157346d242f8a258f2182ea
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 14:33:06 -0600

    Create Timeline.d.ts

---
+++ uds/molecule/Timeline/Timeline.d.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+import { TimelineProps } from './Timeline.types';
+import '../../tailwind.css';
+declare const _default: ((props: TimelineProps) => React.JSX.Element) & {
+    Item: ({ children, label, className, icon, divider, roundIcon, connector, optionalStatus, collapsible, onClick, onCollapse, }: import("./TimelineItem/TimelineItem.types").TimelineItemProps) => React.JSX.Element;
+};
+export default _default;
