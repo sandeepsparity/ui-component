a2c3c7a86c5e614abd5a110346009c3009bd5bf6
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 02:20:20 -0600

    Create Checkbox.d.ts

---
+++ uds/molecule/Checkbox/Checkbox.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { CheckboxProps } from './Checkbox.types';
+import '../../tailwind.css';
+declare const Checkbox: (props: CheckboxProps) => React.JSX.Element;
+export default Checkbox;
