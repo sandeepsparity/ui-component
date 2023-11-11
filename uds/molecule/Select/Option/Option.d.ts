d6a1ebce4cef8dbb161a8422ba0884ecb630c36a
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 13:16:24 -0600

    Create Option.d.ts

---
+++ uds/molecule/Select/Option/Option.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { OptionProps } from './Option.types';
+import '../../../tailwind.css';
+declare const Option: (props: OptionProps) => React.JSX.Element;
+export default Option;
