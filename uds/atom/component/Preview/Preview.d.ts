4ca3c003049b2955dc37079b9b2c48f5eadc8b81
Sandeep Reddy <sandeep.sparity@gmail.com>
2023-11-10 23:37:58 -0600

    Create Preview.d.ts

---
+++ uds/atom/component/Preview/Preview.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { PreviewProps } from './Preview.types';
+import '../../../tailwind.css';
+declare const Preview: (props: PreviewProps) => React.JSX.Element;
+export default Preview;
