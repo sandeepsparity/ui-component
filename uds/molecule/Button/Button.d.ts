60668d96e1cb9d1fb1f7050079b0e9a3b3757b51
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 02:14:43 -0600

    Create Button.d.ts

---
+++ uds/molecule/Button/Button.d.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+import { ButtonProps } from './Button.types';
+import '../../tailwind.css';
+declare const _default: ((props: ButtonProps) => React.JSX.Element) & {
+    Group: (props: import("./ButtonGroup/ButtonGroup.types").ButtonGroupProps) => React.JSX.Element;
+};
+export default _default;
