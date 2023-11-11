d1fb91a43bece6141089c30fd35f768906611e06
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 02:29:16 -0600

    Create Drawer.d.ts

---
+++ uds/molecule/Drawer/Drawer.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { DrawerProps } from './Drawer.types';
+import '../../tailwind.css';
+declare const Drawer: React.ForwardRefExoticComponent<DrawerProps & React.RefAttributes<HTMLDivElement>>;
+export default Drawer;
