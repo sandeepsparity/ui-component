a7e9f9cc83a54b33c72cb74a0bff4d32af05b486
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 00:35:40 -0600

    Create ContentPasteGoIcon.d.ts

---
+++ uds/atom/icon/ContentPasteGoIcon/ContentPasteGoIcon.d.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+import { IconProps } from '../Icon.types';
+declare const ContentPasteGoIcon: (props: IconProps) => React.JSX.Element;
+export default ContentPasteGoIcon;
