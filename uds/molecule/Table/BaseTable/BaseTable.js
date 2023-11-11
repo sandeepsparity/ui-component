a2d324832f5e10015c32925765cef1e6cb522e0b
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 14:08:12 -0600

    Create BaseTable.js

---
+++ uds/molecule/Table/BaseTable/BaseTable.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { BaseTableProps } from './BaseTable.types';
+import '../../../tailwind.css';
+declare const BaseTable: <T>(props: BaseTableProps<T>) => React.JSX.Element;
+export default BaseTable;
