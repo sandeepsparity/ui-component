0f6b3fd16c0ba672c921682c247f9ed35a12acb7
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 14:11:51 -0600

    Create Actions.d.ts

---
+++ uds/molecule/Table/StandardTable/Header/Actions/Actions.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { ActionsProps } from './Actions.types';
+import '../../../../../tailwind.css';
+declare const Actions: <T>(props: ActionsProps<T>) => React.JSX.Element;
+export default Actions;
