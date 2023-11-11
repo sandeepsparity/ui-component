672d7b1558bad1fc7fbb27fae2f177e9a0f682c7
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 14:06:33 -0600

    Create Row.d.ts

---
+++ uds/molecule/Table/BaseTable/Row/Row.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { RowProps } from './Row.types';
+import '../../../../tailwind.css';
+declare const Row: <T>(props: RowProps<T>) => React.JSX.Element;
+export default Row;
