998da7bbc4b377d3ab93a226bb102c5f1ccb9ee3
Sandeep Reddy <sandeep.developersr@gmail.com>
2023-11-11 13:42:33 -0600

    Create ReadyForHandoff.d.ts

---
+++ uds/molecule/Slug/ReadyForHandoff/ReadyForHandoff.d.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+import { ReadyForHandoffProps } from './ReadyForHandoff.types';
+declare const ReadyForHandoff: (props: ReadyForHandoffProps) => React.JSX.Element;
+export default ReadyForHandoff;
